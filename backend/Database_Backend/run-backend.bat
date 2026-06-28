4d8d9887eb4c09944fddd943759299f83b18056d
Mussa Raza <i243022@isb.nu.edu.pk>
2026-06-29 02:59:38 +0500

    Remove documentation and batch files

--- backend/Database_Backend/run-backend.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-:: Run backend project from the solution folder
-:: Usage: run this from backend\Database_Backend: run-backend.bat
-@echo Using project: %~dp0Database_Backend\Database_Backend.csproj
-dotnet restore "%~dp0Database_Backend\Database_Backend.csproj"
-dotnet run --project "%~dp0Database_Backend\Database_Backend.csproj"
--- backend/build.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-cd /d "c:\Users\fireh\OneDrive - FAST National University\Documents\Semester 4\DB Theory\Copy Project\Smart-Disaster-Response-MIS-main\Smart-Disaster-Response-MIS-main\backend\Database_Backend\Database_Backend"
-dotnet build --no-restore
